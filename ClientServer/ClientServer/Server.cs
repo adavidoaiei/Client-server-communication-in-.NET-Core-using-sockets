765efb76b2c74ff0f44c014cffb83aae0856dffa
adavidoaiei <cornel.adavidoaiei@gmail.com>
2017-05-08 13:58:25 +0300

    fix

--- ClientServer/ClientServer/Server.cs
+++ ClientServer/ClientServer/Server.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Net;
+using System.Text;
 using System.Net.Sockets;
 
-namespace Server
+namespace ConsoleApplication
 {
-    class Server
+    class TcpHelper
     {
         private static TcpListener listener { get; set; }
         private static bool accept { get; set; } = false;
-        public static object TcpHelper { get; private set; }
 
         public static void StartServer(int port)
         {
@@ -21,11 +21,46 @@ namespace Server
             Console.WriteLine($"Server started. Listening to TCP clients at 127.0.0.1:{port}");
         }
 
+        public static void Listen()
+        {
+            if (listener != null && accept)
+            {
+
+                // Continue listening.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for client...");
+                    var clientTask = listener.AcceptTcpClientAsync(); // Get the client  
+
+                    if (clientTask.Result != null)
+                    {
+                        Console.WriteLine("Client connected. Waiting for data.");
+                        var client = clientTask.Result;
+                        string message = "";
+
+                        while (message != null && !message.StartsWith("quit"))
+                        {
+                            byte[] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
+                            client.GetStream().Write(data, 0, data.Length);
+
+                            byte[] buffer = new byte[1024];
+                            client.GetStream().Read(buffer, 0, buffer.Length);
+
+                            message = Encoding.ASCII.GetString(buffer);
+                            Console.WriteLine(message);
+                        }
+                        Console.WriteLine("Closing connection.");
+                        client.GetStream().Dispose();
+                    }
+                }
+            }
+        }
+
         public static void Main(string[] args)
         {
             // Start the server  
-            StartServer(1400);
-            Console.ReadKey();
+            TcpHelper.StartServer(443);
+            TcpHelper.Listen(); // Start listening.  
         }
     }
 }
